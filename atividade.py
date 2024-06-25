41f9c9fb752479c7843077d23c4d621df12dcc7d
Jennyfer <ajenny.souza@gmail.com>
2024-06-25 20:46:22 -0300

    Atividade com inicio em while sem true

--- atividade.py
+++ atividade.py
@@ -2,15 +2,18 @@ print("Escolha uma opção: 1- Quantidade de caracteres, 2- Quantidade de palavr
 opcao = int(input())
 
 while opcao < 5:
-    if opcao == 1:
-        print("Informe o texto:")
-        texto = input()
-        print("O tamanho é: " + len(texto))
-    elif opcao == 2:
-        print("Informe o texto: ")
-        frase = input()
-        palavra = frase.split()
-        print("Quantidade de palavras é: " + len(palavra))   
-    elif opcao == 3:
-        print("Você está deixando o sistema.")
-        break
+    if opcao == '1':
+    print("Informe o texto: ")
+    caracter = input()
+    frase_sem_espacos = caracter.replace(" ","")
+    print(f"O tamanho é: {len(frase_sem_espacos)}")
+ 
+  elif opcao == '2':
+    print("Informe a palavra: ")
+    frase = input()
+    quantidade_palavras = frase.split()
+    print(f"Quantidade de palavras é: {len(quantidade_palavras)}")
+ 
+  elif opcao == '3':
+    print("Fechando sistema.")
+    break
